86b7b0b614523e8abf42d4477351efb95408f367
Mauricio Martinez <MauricioAntonioMartinez@outlook.com>
2021-08-28 16:56:47 -0500

    debugg generate token on aws lambda

--- src/auth/generateAccessToken.ts
+++ src/auth/generateAccessToken.ts
@@ -1,5 +1,5 @@
 
-import puppeteer from 'puppeteer-core';
+import { Browser } from "puppeteer";
 import { GenerateAccessTokenParams } from '../types/token';
 
 const TINDER_OAUTH_URL = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&client_id=464891386855067&ret=login&fallback_redirect_uri=221e1158-f2e9-1452-1a05-8983f99f7d6e&ext=1556057433&hash=Aea6jWwMP_tDMQ9y';
@@ -9,34 +9,56 @@ const LOGIN_ID = '#loginbutton';
 const CONFIRM_SELECTOR = 'button[name="__CONFIRM__"]';
 
 async function generateAccessToken({ email, password }:GenerateAccessTokenParams) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  try {
+  let browser:Browser;
 
-  await page.goto(TINDER_OAUTH_URL);
+    if(process.env.PROD){
+      const chromium = require('chrome-aws-lambda');
+      browser =  await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
+    } else {
+      const puppeteer = require("puppeteer");
+       browser = await puppeteer.launch();
+    }
 
-  await page.click(EMAIL_ID);
-  await page.keyboard.type(email);
+    const page = await browser.newPage();
+    
+    await page.goto(TINDER_OAUTH_URL);
+    
+    await page.click(EMAIL_ID);
+    await page.keyboard.type(email);
+    
+    await page.click(PASSWORD_ID);
+    await page.keyboard.type(password);
+    
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(LOGIN_ID),
+    ]);
+    
+    await page.waitForSelector(CONFIRM_SELECTOR);
+    
+    const [response] = await Promise.all([
+      page.waitForResponse(resp => resp.url().endsWith('/dialog/oauth/confirm/')),
+      page.click(CONFIRM_SELECTOR),
+    ]);
+    const responseText = await response.text();
+    const tokenParameter = 'access_token=';
+    const startIndexOfAccessToken = responseText.indexOf(tokenParameter) + tokenParameter.length;
+    const endIndexOfAccessToken = responseText.indexOf('&', startIndexOfAccessToken);
+    await browser.close();
+    return responseText.substring(startIndexOfAccessToken, endIndexOfAccessToken);
+    
+  } catch (error) {
+    console.log(error);
+     throw error
+  } 
 
-  await page.click(PASSWORD_ID);
-  await page.keyboard.type(password);
-
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click(LOGIN_ID),
-  ]);
-
-  await page.waitForSelector(CONFIRM_SELECTOR);
-
-  const [response] = await Promise.all([
-    page.waitForResponse(resp => resp.url().endsWith('/dialog/oauth/confirm/')),
-    page.click(CONFIRM_SELECTOR),
-  ]);
-  const responseText = await response.text();
-  const tokenParameter = 'access_token=';
-  const startIndexOfAccessToken = responseText.indexOf(tokenParameter) + tokenParameter.length;
-  const endIndexOfAccessToken = responseText.indexOf('&', startIndexOfAccessToken);
-  await browser.close();
-  return responseText.substring(startIndexOfAccessToken, endIndexOfAccessToken);
 }
 
 export default generateAccessToken;
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,45 @@
-import { generateToken, refreshCredentials } from "./auth";
-import { getConfiguration, setConfiguration } from "./storage/setKey";
-import { getGirls } from "./tinder/getGirls";
-import { like } from "./tinder/like";
+import { generateToken } from "./auth";
+import { getConfiguration } from "./storage/setKey";
 import { Girl } from "./types/tinder";
 
 
 exports.handler = async ()=>{
     const secretName = process.env.TinderSecretName;
     if(!secretName) throw new Error("Tinder secret name should be set.");
-
-    let config = await getConfiguration(secretName);
-
-    let girls = await getGirls(config.apiToken);
-    let shouldContinue = true;
-    let processedGirls = 0;
- 
-    if(!girls) {
-        config = await refreshCredentials(config)
-        girls = await getGirls(config.apiToken);
-        if (!girls) {
-            config= await generateToken({
-                facebookEmailAddress: process.env.EMAIL!,
-                facebookPassword:process.env.PASSWORD!
-            });
-            girls = await getGirls(config.apiToken);
-            if (!girls) throw new Error("Unable to authenticate on any way");
-            await setConfiguration(secretName,config);
-        }  
-     }
     
-    while (true){
-        shouldContinue = await likeGirls(girls,config.apiToken);
-        if(!shouldContinue) break;
-        processedGirls+= girls.length
-        girls = await getGirls(config.apiToken);
-        if(!girls) break;
-    }
+    let config = await getConfiguration(secretName);
+ 
+    console.log(await generateToken({
+        facebookEmailAddress: process.env.EMAIL!,
+        facebookPassword:process.env.PASSWORD!
+    }))
+    // let girls = await getGirls(config.apiToken);
+    // let shouldContinue = true;
+    // let processedGirls = 0;
+ 
+    // if(!girls) {
+    //     config = await refreshCredentials(config)
+    //     girls = await getGirls(config.apiToken);
+    //     if (!girls) {
+    //         config= await generateToken({
+    //             facebookEmailAddress: process.env.EMAIL!,
+    //             facebookPassword:process.env.PASSWORD!
+    //         });
+    //         girls = await getGirls(config.apiToken);
+    //         if (!girls) throw new Error("Unable to authenticate on any way");
+    //         await setConfiguration(secretName,config);
+    //     }  
+    //  }
+    
+    // while (true){
+    //     shouldContinue = await likeGirls(girls,config.apiToken);
+    //     if(!shouldContinue) break;
+    //     processedGirls+= girls.length
+    //     girls = await getGirls(config.apiToken);
+    //     if(!girls) break;
+    // }
 
-    console.log(`Ending processing girls(likes), liked ${processedGirls}`);
+    // console.log(`Ending processing girls(likes), liked ${processedGirls}`);
 }    
 
 
@@ -45,8 +47,8 @@ exports.handler = async ()=>{
 
 const likeGirls = async (girls:Girl[],token:string)=>{ 
     for(const girl of girls){ 
-        const ok  = await like(girl.user._id,token); 
-        if (!ok) return false
+        // const ok  = await like(girl.user._id,token); 
+        // if (!ok) return false
         console.log(`Liked ${girl.user.name}`);
     }
     return true;
