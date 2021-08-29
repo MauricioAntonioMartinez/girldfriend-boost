7100903a350b3f476256774ca0df5092c2087d3f
Mauricio Martinez <MauricioAntonioMartinez@outlook.com>
2021-08-29 14:28:33 -0500

    result of like

--- src/tinder/like.ts
+++ src/tinder/like.ts
@@ -13,6 +13,7 @@ export const like = async(girlId:string,token:string):Promise<boolean> =>{
             console.log(`Rate limited until ${new Date(data.rate_limited_until).toLocaleString()}`)
             return false
         }
+        console.log(data);
 
         return true
     } catch (error :any) {
